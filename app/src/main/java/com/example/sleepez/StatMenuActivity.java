6ef9990e1a3d7a3f3d0e6da735625c40e8ffc01f
Nate Kaval <natekav26@gmail.com>
2022-12-08 17:59:08 -0500

    Updated stats page

--- app/src/main/java/com/example/sleepez/StatMenuActivity.java
+++ app/src/main/java/com/example/sleepez/StatMenuActivity.java
@@ -3,6 +3,9 @@ package com.example.sleepez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class StatMenuActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class StatMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stat_menu);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.stat_select_array, R.layout.stat_spinner_text);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner;
+        spinner = findViewById(R.id.stat_spinner);
+        spinner.setAdapter(adapter);
     }
 }
