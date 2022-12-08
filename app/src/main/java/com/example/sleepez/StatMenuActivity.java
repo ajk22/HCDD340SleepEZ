6b55d494c2091b51c7ecbefffbf2580761bc8e92
Nate Kaval <natekav26@gmail.com>
2022-12-08 18:27:42 -0500

    Added click event to stats page

--- app/src/main/java/com/example/sleepez/StatMenuActivity.java
+++ app/src/main/java/com/example/sleepez/StatMenuActivity.java
@@ -3,11 +3,14 @@ package com.example.sleepez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class StatMenuActivity extends AppCompatActivity {
+public class StatMenuActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button showStatBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +22,18 @@ public class StatMenuActivity extends AppCompatActivity {
         Spinner spinner;
         spinner = findViewById(R.id.stat_spinner);
         spinner.setAdapter(adapter);
+
+        showStatBtn = findViewById(R.id.stat_show_btn);
+        showStatBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int eventSourceId = view.getId();
+        if (eventSourceId == R.id.stat_show_btn) {
+            Spinner spinner = findViewById(R.id.stat_spinner);
+            String range = (String) spinner.getSelectedItem();
+            Log.d("Stat_Test", "Selected range: " + range);
+        }
     }
 }
