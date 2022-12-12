c3264ad0ff6af72660d126e6f5c0f4a7035b4d1f
Nate Kaval <natekav26@gmail.com>
2022-12-11 19:59:27 -0500

    Fixed layout of login screen and made it the main intent

---
+++ app/src/main/java/com/example/sleepez/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.sleepez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity implements  View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.loginBtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.loginBtn){
+            Intent mainMenuIntent = new Intent(this, MainActivity.class);
+            startActivity(mainMenuIntent);
+        }
+    }
+}
