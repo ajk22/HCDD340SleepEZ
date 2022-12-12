540f48c88d7b1e635d041c1e5724d8ab55099638
Chris Moreno <cjm6771@i4-lx-android.ad.psu.edu>
2022-12-12 06:18:44 +0000

    Added button functionality to goals activity and began the new goal activity

---
+++ app/src/main/java/com/example/sleepez/NewGoalActivity.java
@@ -0,0 +1,20 @@
+package com.example.sleepez;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewGoalActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
